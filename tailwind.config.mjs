ee28002f574fb9e131d39c50b9927cbe972b73bd
Mauro Risso <73178418+y0mauro@users.noreply.github.com>
2024-05-24 19:26:29 +0200

    vercel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme'
 
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -16,6 +17,20 @@ export default {
 
 
 			},
+		},
+		animation: {
+			marquee: 'marquee 10s linear infinite',
+			marquee2: 'marquee2 10s linear infinite',
+		},
+		keyframes: {
+			marquee: {
+				'0%': { transform: 'translateX(0%)' },
+				'100%': { transform: 'translateX(-100%)' },
+			},
+			marquee2: {
+				'0%': { transform: 'translateX(100%)' },
+				'100%': { transform: 'translateX(0%)' },
+			}
 		},
 	},
 	plugins: [],
