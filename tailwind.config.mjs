27838d9e2ebe725ec4fbdd6412d68cb9cc1d5c15
Mauro Risso <73178418+y0mauro@users.noreply.github.com>
2024-06-10 21:27:35 +0200

    bug fixed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,21 +17,22 @@ export default {
 
 
 			},
-		},
-		animation: {
-			marquee: 'marquee 10s linear infinite',
-			marquee2: 'marquee2 10s linear infinite',
-		},
-		keyframes: {
-			marquee: {
-				'0%': { transform: 'translateX(0%)' },
-				'100%': { transform: 'translateX(-100%)' },
+			animation: {
+				marquee: 'marquee 10s linear infinite',
+				marquee2: 'marquee2 10s linear infinite',
 			},
-			marquee2: {
-				'0%': { transform: 'translateX(100%)' },
-				'100%': { transform: 'translateX(0%)' },
-			}
+			keyframes: {
+				marquee: {
+					'0%': { transform: 'translateX(0%)' },
+					'100%': { transform: 'translateX(-100%)' },
+				},
+				marquee2: {
+					'0%': { transform: 'translateX(100%)' },
+					'100%': { transform: 'translateX(0%)' },
+				}
+			},
 		},
+		
 	},
 	plugins: [],
 }
